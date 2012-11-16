f8137c8bb5c779a4fa7ed83aa3d3de7d5db9a935
Joakim Stien <joakim.stien@gmail.com>
2012-11-16 23:28:43 +0100

    Updated slightly.

--- Trollberg/Weapon.cpp
+++ Trollberg/Weapon.cpp
@@ -1,8 +1,78 @@
 #include "Weapon.h"
 
 
-Weapon* Weapon::createWeapon(WeaponID wid)
+Weapon* Weapon::createWeapon(Pim::SpriteBatchNode *b, WeaponID wid)
 {
+	switch (wid)
+	{
+		case LIGHT_RIFLE:
+			return new LightRifle(b);
+	}
+
 	return NULL;
 }
+
+Weapon::Weapon(Pim::SpriteBatchNode *b)
+	: Pim::Sprite()
+{
+	actorSheet = b;
+	useBatchNode(actorSheet);
+}
+
+float Weapon::damage()
+{
+	// Get a random number between 0 and 1
+	float r = rand()/RAND_MAX;
+	return minDamage + r*(maxDamage-minDamage);
+}
+float Weapon::angle()
+{
+	// Get a random number between -1 and 1
+	float r = (rand()/RAND_MAX - 0.5)*2;
+	return rotation + r*accuracy;
+}
+
+
+// ----------- LIGHT RIFLE ----------- //
+LightRifle::LightRifle(Pim::SpriteBatchNode *b)
+{
+	accuracy  = 10.f;
+	minDamage = 20;
+	maxDamage = 30;
+}
+
+void LightRifle::fire()
+{
+
+}
+void LightRifle::setMirrored(bool flag)
+{
+	scale.y = 1.f - (float)flag*2.f;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- Trollberg/Weapon.h
+++ Trollberg/Weapon.h
@@ -9,16 +9,32 @@ enum WeaponID
 class Weapon : public Pim::Sprite
 {
 public:
-	static Weapon* createWeapon(WeaponID);
+	static Weapon* createWeapon(Pim::SpriteBatchNode *b, WeaponID);
+
+	virtual float damage();		// Returns a calculated damage value
+	virtual float angle();		// Returns a calculated fire angle
+
+	virtual void fire() = 0;	// Fire the bullet. 
+	virtual void setMirrored(bool flag) = 0;
 
 protected:
-	Weapon() {}
+	// Private constructors for the sake of fuck you.
+	Weapon(Pim::SpriteBatchNode *batch);
 	Weapon(const Weapon&) {}
+
+	Pim::SpriteBatchNode 	*actorSheet;
+	int 	minDamage;	// The minimum damage
+	int		maxDamage;	// The maximum damage
+	float 	accuracy;	// +/-  degrees 
+	float 	rof;		// Rate of fire - bullets per second
 };
 
 
 class LightRifle : public Weapon
 {
+	LightRifle(Pim::SpriteBatchNode *batch);
 
+	void fire();
+	void setMirrored(bool flag);
 };
 
