859e51748236a99799bfcc77ee80796e2ffa6d10
Jan Greger Hemb <jan.greger@gmail.com>
2012-11-21 23:09:30 +0100

    Ny Meny

--- Trollberg/MainMenuLayer.cpp
+++ Trollberg/MainMenuLayer.cpp
@@ -1,7 +1,9 @@
 #pragma once 
+#include "MenuButton.h"
 #include "MainMenuLayer.h"
 #include "MainMenuScene.h"
 #include "GameScene.h"
+#include "Pim.h"
 
 
 MainMenuLayer::MainMenuLayer()
@@ -15,16 +17,24 @@ void MainMenuLayer::loadResources()
 	buttonSheet = new Pim::SpriteBatchNode("res/buttonsMainMenu.png");
     addChild(buttonSheet);
 
-	for(int i = 0; i < 4; i ++)
+	std::string menyButtonLabels[NUMMENYBUTTONS]  = {"Start", "Options", "Highscore", "Exit"};
+
+	for(int i = 0; i < NUMMENYBUTTONS; i ++)
 	{
-		mainButton = createButton(192, 108 - (PTMR*i*3));
+		std::cout << "i: " << i << " verdi: " << menyButtonLabels[i];
+		menuButtonsArray[i] = createButton(192, 108 - (PTMR*i*3), menyButtonLabels[i]);
 	}
+
+	menuButtonsArray[0]->setZOrder(0);
+
+	listenFrame();
+
 }
 
-Pim::Button* MainMenuLayer::createButton(int xPos, int yPos)
+MenuButton* MainMenuLayer::createButton(int xPos, int yPos, std::string buttonLabel)
 {
     Pim::Sprite *normal = new Pim::Sprite;
-    normal->useBatchNode(buttonSheet); 
+    normal->useBatchNode(buttonSheet) ; 
     normal->rect = Pim::Rect(0,0,40,25);
 
     Pim::Sprite *hovered = new Pim::Sprite;
@@ -35,19 +45,49 @@ Pim::Button* MainMenuLayer::createButton(int xPos, int yPos)
     pressed->useBatchNode(buttonSheet); 
     pressed->rect = Pim::Rect(80,0,40,25);
 
-    Pim::Button *button = new Pim::Button(normal, hovered, pressed, NULL);
-    button->setCallback(this);
-    addChild(button);
+	Pim::Font *fonten =  new Pim::Font("res/arial.ttf", 50, true);
+
+	MenuButton *menuButton = new MenuButton(normal, hovered, pressed, fonten);
+	addChild(menuButton);
+	menuButton->position = Pim::Vec2(192, 108);
+	menuButton->setText(buttonLabel);
+
+    menuButton->setCallback(this);
 
 	//button->position = Pim::Vec2(192, 108);
 	//button->position = Pim::Vec2(rand()%384, rand()%216);
-	button->position = Pim::Vec2(xPos, yPos);
+	menuButton->position = Pim::Vec2(xPos, yPos);
 
 
-	return button;
+	return menuButton;
 }
 
-void MainMenuLayer::buttonPressed(Pim::Button*)
+void MainMenuLayer::buttonPressed(Pim::Button* activeButton)
 {
-	Pim::GameControl::getSingleton()->setScene(new GameScene(1));
+
+	if(activeButton == menuButtonsArray[0]) //start
+	{
+		Pim::GameControl::getSingleton()->setScene(new GameScene(1));
+	}
+	if(activeButton == menuButtonsArray[3]) //Options
+	{
+		exit(0);
+	}
+	if(activeButton == menuButtonsArray[3]) //Highscore
+	{
+		exit(0);
+	}
+	if(activeButton == menuButtonsArray[3]) //Exit
+	{
+		exit(0);
+	}
+	
+	//Pim::GameControl::getSingleton()->setScene(new GameScene(1));
+}
+
+void MainMenuLayer::update(float dt)
+{
+
+	//menuButtonsArray[0]->position = Pim::Vec2(rand()%SCREENWIDTH, rand()%SCREENHEIGHT);
+
 }
--- Trollberg/MainMenuLayer.h
+++ Trollberg/MainMenuLayer.h
@@ -1,5 +1,7 @@
 #pragma once 
 #include "Pim.h"
+#include "MenuButton.h"
+#define NUMMENYBUTTONS 4
 
 class MainMenuLayer: public Pim::Layer, public Pim::ButtonCallback
 {
@@ -8,12 +10,18 @@ public:
 
 	void loadResources();
 
-	Pim::Button* createButton(int xPos, int yPos);
+	MenuButton* createButton(int xPos, int yPos, std::string buttonLabel);
 
 	void buttonPressed(Pim::Button*);
+
+	void MainMenuLayer::update(float dt);
 
 private:
 	Pim::SpriteBatchNode *buttonSheet;
 
 	Pim::Button *mainButton;
+
+	MenuButton *menuButtonsArray[NUMMENYBUTTONS];
+
+	MenuButton *menuButtonTest;
 };
