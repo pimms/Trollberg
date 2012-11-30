0d33a6a9872bdffd182cec7feb8b3ef46aa73b24
pimms <joakim.stien@gmail.com>
2012-11-30 16:37:53 +0100

    Removed libCURL

--- Trollberg/Trollberg.cpp
+++ Trollberg/Trollberg.cpp
@@ -2,15 +2,8 @@
 #include "GameScene.h"
 #include "MainMenuScene.h"
 
-#include "OLLeaderboard.h"
-
 int main()
 {
-	OLLeaderboard leader;
-	leader.connect();
-
-	//system("pause"); 
-
 	{   // Stack cheating: avoid 'cd' from "leaking" memory
 		Pim::WinStyle::CreationData cd;
 
